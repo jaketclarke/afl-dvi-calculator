dbcc2334f8a4d53420696d63f80ff4a7cea7e7a9
Jake Clarke <jake.t.clarke@gmail.com>
2021-06-08 22:36:27 +1000

    adding in and out

--- src/App.js
+++ src/App.js
@@ -3,33 +3,58 @@ import './App.css';
 
 const App = () => {
 const [pickIn, setPickIn] = React.useState([])
+const [pickOut, setPickOut] = React.useState([])
 const dataJson = require('./dvi.json')
 // const data = JSON.parse(dataJson)
 const data = dataJson
 
-const handleSubmit = (event) => {
+const handleSubmitIn = (event) => {
   event.preventDefault()
-  console.log(dataJson)
   const data = new FormData(event.target)
   const pickIn = data.get(`pickIn`)
   // maybe a string of string do Number(data.get(‘pickIn’)
   setPickIn(prev => [...prev, Number(data.get('pickIn'))])
 
 }
-  const totalPoints = pickIn.reduce((acc,cur) => {
+
+const handleSubmitOut = (event) => {
+  event.preventDefault()
+  const data = new FormData(event.target)
+  const pickIn = data.get(`pickOut`)
+  // maybe a string of string do Number(data.get(‘pickIn’)
+  setPickOut(prev => [...prev, Number(data.get('pickOut'))])
+
+}
+
+
+  const totalPointsIn = pickIn.reduce((acc,cur) => {
+    const points = data.find(pick => pick.pick === cur).points
+    return acc + points 
+    },0)
+
+  const totalPointsOut = pickOut.reduce((acc,cur) => {
     const points = data.find(pick => pick.pick === cur).points
     return acc + points 
     },0)
 
     
-  
     return <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmitIn}>
         <input name='pickIn' type='number' />
         <button>Add</button>
-    </form>
-    {pickIn.map((pick,index) => <p key={index}>{pick}</p>)}
-    <p>Total Points: {totalPoints}</p>
+      </form>
+
+      <form id = '2' onSubmit={handleSubmitOut}>
+        <input name='pickOut' type='number' />
+        <button>Add</button>
+      </form>
+
+      {pickIn.map((pick,index) => <p key={index}>{pick}</p>)}
+      <p>Total Points In: {totalPointsIn}</p>
+
+      {pickOut.map((pick,index) => <p key={index}>{pick}</p>)}
+      <p>Total Points Out: {totalPointsOut}</p> 
+
     </>
   
 
