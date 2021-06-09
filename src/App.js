7e7b13fd17bc77e7598ec9a005785f8849024cb3
Jake Clarke <jake.t.clarke@gmail.com>
2021-06-09 16:23:07 +1000

    clean up unused imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Link from '@material-ui/core/Link';
-import MailOutlineIcon from '@material-ui/icons/MailOutlined';
-import { FaGit, FaGithub } from "react-icons/fa";
-import { ButtonBase, Icon } from '@material-ui/core';
+import {  FaGithub } from "react-icons/fa";
 
 // work out how to use these!
 const useStyles = makeStyles((theme) => ({
@@ -75,14 +68,14 @@ const App = () => {
   const handleSubmitIn = (event) => {
     event.preventDefault()
     const data = new FormData(event.target)
-    const pickIn = data.get(`pickIn`)
+    // const pickIn = data.get(`pickIn`)
     setPickIn(prev => [...prev, Number(data.get('pickIn'))])
   }
 
   const handleSubmitOut = (event) => {
     event.preventDefault()
     const data = new FormData(event.target)
-    const pickOut = data.get(`pickOut`)
+    // const pickOut = data.get(`pickOut`)
     setPickOut(prev => [...prev, Number(data.get('pickOut'))])
   }
 
